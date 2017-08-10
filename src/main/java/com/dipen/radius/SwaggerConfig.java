2f76ca8ca6f1d9d0369a2773e2f753b6a57a8f73
Dipen Lama <lamadipen@yahoo.com>
2017-08-10 11:38:52 -0500

    closed #5 implemented swagger in the project added dependency in pom.xml and created a SwaggerConfig class

---
+++ src/main/java/com/dipen/radius/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.dipen.radius;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.spring.web.plugins.Docket;;
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public  Docket api()
+	{
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+	
+}
+/* Swagger Resource
+ * http://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
+ * */
+ 
