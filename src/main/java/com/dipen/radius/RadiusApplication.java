e5458eccea2dfa857b79fba33ccc3bc2329cb847
Dipen Lama <lamadipen@yahoo.com>
2017-08-10 13:35:04 -0500

    closed #6 applied schedualr in thespring boot application with annoation and adding class for schudled task

--- src/main/java/com/dipen/radius/RadiusApplication.java
+++ src/main/java/com/dipen/radius/RadiusApplication.java
@@ -2,10 +2,12 @@ package com.dipen.radius;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+//@EnableScheduling //uncomment the line to run scheduler on you application
 public class RadiusApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/dipen/radius/schedular/ScheduledTasks.java
@@ -0,0 +1,22 @@
+package com.dipen.radius.schedular;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+  private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+  
+  private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+  
+  @Scheduled(fixedRate = 1000)
+  public void reporteCurrentTime()
+  {
+	  logger.info("The time is now {}", dateFormat.format(new Date()));
+  }
+}
